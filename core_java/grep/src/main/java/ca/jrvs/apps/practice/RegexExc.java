ae7c65eb0819c7538ce3cf73123f6e945d72ae3b
Shen Wang <shen1091035694@gmail.com>
2024-06-07 20:48:38 +0000

    regex practice

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
@@ -0,0 +1,26 @@
+package main.java.ca.jrvs.apps.practice;
+
+public interface RegexExc {
+
+    /**
+     * Return true if filename extension is jpg or jpeg( case in_sensitive)
+     * @param filename - a file name stored as String
+     * @return boolean
+     */
+    boolean matchJpeg(String filename);
+
+    /**
+     * return true if ip is valid
+     * to simplify the problem, IP address range is from 0.0.0.0 to 999.999.999.999
+     * @param ip - an IP stored as String
+     * @return boolean
+     */
+    boolean matchIP(String ip);
+
+    /**
+     * return true if line is empty (e.g. empty, white space, tabs, etc..)
+     * @param line - A String
+     * @return boolean
+     */
+    boolean isEmptyLine(String line);
+}
